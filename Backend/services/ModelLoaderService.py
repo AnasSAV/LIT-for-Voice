42bdc9f850fc2d724b1abf083ed0e5725c3bca9d
Chand2103 <chandupaambepitiya@gmail.com>
2025-08-11 13:43:41 +0530

    integrated whisper

---
+++ Backend/services/ModelLoaderService.py
@@ -0,0 +1,38 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+).to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+
+sample = "sample.wav"
+
+generate_kwargs = {
+    "return_timestamps": True,
+}
+
+result = pipe(
+    sample,
+    generate_kwargs=generate_kwargs,
+    chunk_length_s=30,  # prevents mel features > 3000
+    batch_size=8
+)
+print(result["text"])
